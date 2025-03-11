c6e91d0221281684378261f696362ba22d2610d2
vitor sampaio <vitorsampaio.tds2@gmail.com>
2025-03-11 14:43:14 -0300

    feat: create database

---
+++ prisma/migrations/20250311173540_create_table/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "descricao" TEXT NOT NULL,
+    "concluida" BOOLEAN NOT NULL DEFAULT false,
+    "criadaEm" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
